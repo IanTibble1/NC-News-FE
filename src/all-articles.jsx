a85da23e1c62330508eb82dc9a2dbc8a01ba91d9
IanTibble1 <iantibble1@gmail.com>
2023-09-04 12:12:30 +0100

    adds article list and styling

--- src/all-articles.jsx
+++ src/all-articles.jsx
@@ -1,11 +1,33 @@
 import Header from "./components/header";
 import NavBar from "./components/navBar";
+import ArticleList from "./components/article-list";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const AllArticles = () => {
+  const [articlesList, setArticlesList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://news-server-1ny4.onrender.com/api/articles")
+      .then(({ data }) => {
+        setArticlesList(data.articles);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <main>
       <Header />
       <NavBar />
+      <button>Topics filler</button>
+      <div className="article-grid">
+        {articlesList.map((article) => (
+          <ArticleList key={article.article_id} article={article} />
+        ))}
+      </div>
     </main>
   );
 };
