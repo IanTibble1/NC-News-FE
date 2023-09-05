740d354226840aee051df6400c2257be8d05534c
IanTibble1 <iantibble1@gmail.com>
2023-09-05 11:29:03 +0100

    adds vote increase button functionality

--- src/components/article-card.jsx
+++ src/components/article-card.jsx
@@ -1,7 +1,21 @@
 import moment from "moment";
+import axiosBase from "./axios-base";
+import { useParams } from "react-router-dom";
 
 const ArticleCard = ({ article }) => {
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    axiosBase
+      .patch(`articles/${article.article_id}`, { inc_vote: 1 })
+      .then(() => {})
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   const formatDate = moment(article.created_at).utc().format("DD-MM-YYYY");
+
   return (
     <main>
       <section className="article-card">
@@ -22,7 +36,9 @@ const ArticleCard = ({ article }) => {
           </p>
           <div className="vote-section">
             <p>Votes: {article.votes}</p>
-            <button>Add vote</button>
+            <button onClick={handleClick} type="submit">
+              Add vote
+            </button>
           </div>
         </div>
       </section>
