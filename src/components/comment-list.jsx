1374b6b2cd64fa108b945ce1edcb556de4c5aaec
IanTibble1 <iantibble1@gmail.com>
2023-09-05 10:42:49 +0100

    adds isLoading to single article and commentList

--- src/components/comment-list.jsx
+++ src/components/comment-list.jsx
@@ -4,17 +4,23 @@ import CommentCard from "./comment-card";
 
 const CommentList = ({ article_id }) => {
   const [commentList, setCommentList] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
+    setIsLoading(true);
     axiosBase.get(`/articles/${article_id}/comments`).then(({ data }) => {
       setCommentList(data.comments);
+      setIsLoading(false);
     });
   }, []);
   return (
     <section>
-      {commentList.map((comment) => (
-        <CommentCard key={comment.comment_id} comment={comment} />
-      ))}
+      {isLoading ? (
+        <p className="loading">Loading...</p>
+      ) : (
+        commentList.map((comment) => (
+          <CommentCard key={comment.comment_id} comment={comment} />
+        ))
+      )}
     </section>
   );
 };
