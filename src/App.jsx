08774a75faaaa48299efa76cc6435c644ef87fad
IanTibble1 <iantibble1@gmail.com>
2023-09-07 12:28:22 +0100

    adds 'page does not exist' error page to incorrect url

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import HomePage from "./HomePage";
 import AllArticles from "./AllArticles";
 import SingleArticle from "./SingleArticle";
+import ErrorPage from "./ErrorPage";
 
 function App() {
   return (
     <main>
       <Routes>
+        <Route path="*" element={<ErrorPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/articles" element={<AllArticles />} />
         <Route path="/articles/:articleId" element={<SingleArticle />} />
